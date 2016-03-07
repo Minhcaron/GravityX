49a5f8c4753708e24f58af421f4695e8911e7117
Eduardo Lopez <eduardo@Eduardos-MacBook-Pro.local>
2016-03-07 16:48:22 -0500

    Main class
    Create the main class.

---
+++ src/GravityX.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Eduardo Lopez
+ *
+ */
+public class GravityX {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+
+	}
+
+}
